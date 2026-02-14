58fa7ea57f4eb4e626a5432f5fa90a78d016e2ff
sukangpunch <popora99@naver.com>
2026-02-14 22:34:40 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/example/talentoftime/TalentOfTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.talentoftime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentOfTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentOfTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/talentoftime/TalentOfTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talentoftime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentOfTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
